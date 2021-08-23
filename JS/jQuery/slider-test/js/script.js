4d5dc272c3fb79b6f701824ea577911743bb4d4f
まっきー <81737622+yuseipen0716@users.noreply.github.com>
2021-08-23 20:36:56 +0900

    Add files via upload

---
+++ JS/jQuery/slider-test/js/script.js
@@ -0,0 +1,20 @@
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+	autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+	speed:1000,//スライドの動きのスピード。初期値は300。
+	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 1,//スライドを画面に3枚見せる
+	slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+	arrows: true,//左右の矢印あり
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	dots: true,//下部ドットナビゲーションの表示
+    pauseOnFocus: false,//フォーカスで一時停止を無効
+    pauseOnHover: false,//マウスホバーで一時停止を無効
+    pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
+
+//スマホ用
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+    $('.slider').slick('slickPlay');
+});
